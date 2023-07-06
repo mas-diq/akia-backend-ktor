a71de3967e7d82cc213d112599343694e8a5207a
Muhammad Shiddiq Fathullah <shiddiqmu4@gmail.com>
2023-07-06 14:01:28 +0700

    Create connection string to Mongo DB

--- src/main/kotlin/com/masdiq/repository/MongoDb.kt
+++ src/main/kotlin/com/masdiq/repository/MongoDb.kt
@@ -3,6 +3,6 @@ package com.masdiq.repository
 import org.litote.kmongo.coroutine.coroutine
 import org.litote.kmongo.reactivestreams.KMongo
 
-val client = KMongo.createClient().coroutine
-val DATABASE = client.getDatabase("Akia")
+val client = KMongo.createClient("mongodb+srv://masdiq:B76bnyQkRz2VJR0s@cluster0.qhwa22p.mongodb.net/?retryWrites=true&w=majority").coroutine
+val DATABASE = client.getDatabase("akia_database")
 
